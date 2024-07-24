3f864679513ce58f26202f3378f319a47d842029
David Cedres <david.cedresx@gmail.com>
2024-07-23 20:30:11 -0400

    add sede control with state

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,21 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { Button, Container, Input, Stack, Title } from '@mantine/core'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { Button, Container, Input, Stack, Title } from "@mantine/core";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [sede, setSede] = useState("pepe");
+
+  const manejarSubmit = () => {
+    // Aquí se debe enviar la sede al backend
+
+    console.log("Sede:", sede);
+  };
 
   return (
     <Container>
       <Stack>
         <Title>Aguegue las sedes</Title>
-        <Input placeholder='Escriba la sede...'/>
-        <Input placeholder='Escriba la extensión...'/>
-        <Button>Guardar</Button>
+        <Input
+          placeholder="Escriba la sede..."
+          value={sede}
+          onChange={(e) => setSede(e.target.value)}
+        />
+
+        <Input placeholder="Escriba la extensión..." />
+
+        <Button onClick={manejarSubmit}>Guardar</Button>
       </Stack>
     </Container>
-  )
+  );
 }
 
-export default App
+export default App;
