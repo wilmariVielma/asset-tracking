3c4655da73a45edf9d2279d6bc023fbb7f938b7e
wilmarivielma <wilmarivielma1@gmail.com>
2024-07-23 20:19:28 -0400

    mantine

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,10 +1,14 @@
+import '@mantine/core/styles.css';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+
+import { createTheme, MantineProvider } from '@mantine/core';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider>
+      <App />
+    </MantineProvider>
   </React.StrictMode>,
 )
