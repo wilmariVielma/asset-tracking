8bf2a1cd2e6609ed9b83347ba944c18e91d8bd05
wilmarivielma <wilmarivielma1@gmail.com>
2024-07-23 20:55:47 -0400

    guardamos la información

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,27 +2,38 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import { Button, Container, Input, Stack, Title } from "@mantine/core";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 function App() {
-  const [sede, setSede] = useState("pepe");
+  const [sede, setSede] = useState("");
+  const [extension, setExtension] = useState("");
 
   const manejarSubmit = () => {
     // Aquí se debe enviar la sede al backend
+    axios.post("http://localhost:3000/sedes", {sede, extension})
 
-    console.log("Sede:", sede);
+    toast.success("¡Guardado con éxito!")
+
+    setSede("")
+    setExtension("")
+
   };
 
   return (
     <Container>
       <Stack>
-        <Title>Aguegue las sedes</Title>
+        <Title>Aguegue la sede</Title>
         <Input
           placeholder="Escriba la sede..."
           value={sede}
           onChange={(e) => setSede(e.target.value)}
         />
 
-        <Input placeholder="Escriba la extensión..." />
+        <Input placeholder="Escriba la extensión..." 
+        value={extension}
+        onChange={(e) => setExtension(e.target.value)}
+        />
 
         <Button onClick={manejarSubmit}>Guardar</Button>
       </Stack>
--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -4,11 +4,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
 import { createTheme, MantineProvider } from '@mantine/core';
+import { Toaster } from 'react-hot-toast';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <MantineProvider>
       <App />
+      <Toaster />
     </MantineProvider>
   </React.StrictMode>,
 )
