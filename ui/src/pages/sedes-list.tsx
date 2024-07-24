680fca0ea38aba4a429af523b20945feb7c01e21
wilmarivielma <wilmarivielma1@gmail.com>
2024-07-23 22:35:58 -0400

    se sgregó lista de sedes

--- ui/src/pages/sedes-list.tsx
+++ ui/src/pages/sedes-list.tsx
@@ -1,12 +1,47 @@
-import { Stack, Title } from "@mantine/core";
+import { Stack, Table, Title } from "@mantine/core";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const SedesList = () => {
+  
+  const [sedes, setSedes] = useState([]);
+
+  useEffect(() => {
+
+
+
+    axios
+      .get("http://localhost:3000/sedes")
+      .then((response) => setSedes(response.data));
+
+
+  
+  }, [])
+
+  const rows = sedes.map((element) => (
+    <Table.Tr key={element.name}>
+      <Table.Td>{element.sede}</Table.Td>
+      <Table.Td>{element.extension}</Table.Td>
+    </Table.Tr>
+  ));
+
   return (
     <Stack>
       <Title>Mis Sedes</Title>
 
       <Link to="/sedes/new">Agregar nueva sede</Link>
+
+      <Table>
+      <Table.Thead>
+        <Table.Tr>
+          <Table.Th>Sede</Table.Th>
+          <Table.Th>Extensión</Table.Th>
+        </Table.Tr>
+      </Table.Thead>
+      <Table.Tbody>{rows}</Table.Tbody>
+    </Table>
+
     </Stack>
   );
 };
